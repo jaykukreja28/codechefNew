0b7ab623e8dd0c2d556d743958d55c357b3bcbf3
Sir Jay Kukreja <chunty@bunty.com>
2021-04-24 23:11:57 +0530

    Comments added

---
+++ Luckyfour.java
@@ -0,0 +1,3 @@
+/*
+Impressed by the power of this number, Karan has begun to look for occurrences of four anywhere. He has a list of T integers, for each of them he wants to calculate the number of occurrences of the digit 4 in the decimal representation. He is too busy now, so please help him
+*/
